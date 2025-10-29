488f133c7a1d2345f97dbb226d94586a7e1c8b22
Leonidas2121 <panleon21@gmail.com>
2025-10-29 21:12:27 +0200

    changes2

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,14 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    {/* Το BASE_URL λέει στο router να χρησιμοποιεί /LM_ANAKAINISI/ όταν είσαι στο GitHub Pages */}
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
